f7cabb44b0de45073b94f25e1ffa80763852a8a9
Roberto Gomez <robertoezequiel@live.com.ar>
2024-01-24 22:40:20 -0300

    Se agrego el middleware Not Found a index.js

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require("express");
 const server = express();
 const {PORT} = require("./config");
 const {HomeRoutes} = require("./routes");
+const {NotFoundMiddleware} = require("./middlewares");
 
 server.use(express.static("./public"));
 server.use(express.json());
 
 server.use("/", HomeRoutes);
+server.use(NotFoundMiddleware)
 
 server.listen(PORT, () => {
     console.log("Entro por el puerto " + PORT);
