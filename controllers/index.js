7c9a56ad77824a233d6f01b626d92d66439cad34
Roberto Gomez <robertoezequiel@live.com.ar>
2024-01-30 22:37:27 -0300

    Se crearon los controllers y routes necesarios faltantes del proyecto

--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,4 @@
 module.exports = {
-    HomeController: require("./home.controller")
+    HomeController: require("./home.controller"),
+    QuotesController: require("./quotes.controller")
 };
--- routes/home.routes.js
+++ routes/home.routes.js
@@ -3,5 +3,6 @@ const {HomeController} = require("../controllers");
 const homeController = require("../controllers/home.controller");
 
 router.get("/", homeController.index);
+router.get("/about", homeController.about);
 
 module.exports = router;
